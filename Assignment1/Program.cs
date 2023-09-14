22f0734d59d019bd268b76749b72cf1dbd57663c
Minh Christian Tran <54357939+Mintra99@users.noreply.github.com>
2023-09-14 12:57:15 +0200

    edited program.cs

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -7,23 +7,6 @@ namespace DungeonMasterApp
         static void Main(string[] args)
         {
 
-            // Create a wizard
-            Wizard wizard = new Wizard("WizardName");
-            Weapon LargeRod = new Weapon("Large rod", 5, Slot.Weapon, WeaponType.Staffs, 70);
-            Armor NullMagicMantle = new Armor("Null-Magic Mantle", 1, Slot.Body, ArmorType.Cloth, new HeroAttribute(0, 2, 0));
-            Armor NinjaTabi = new Armor("Ninja tabi", 1, Slot.Legs, ArmorType.Cloth, new HeroAttribute(0, 2, 2));
-            Armor Deathcap = new Armor("Rabadons deathcap", 5, Slot.Head, ArmorType.Cloth, new HeroAttribute(0, 2, 10));
-            wizard.LevelUp();
-            wizard.LevelUp();
-            wizard.LevelUp();
-            wizard.LevelUp();
-            wizard.EquipWeapon(LargeRod);
-            wizard.EquipArmor(NullMagicMantle);
-            wizard.EquipArmor(NinjaTabi);
-            wizard.EquipArmor(Deathcap);
-
-            Console.WriteLine("\nWizard Details:");
-            wizard.Display();
         }
     }
 }
